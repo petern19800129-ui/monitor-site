55898418d2cf0aef611952a1530e1b73f920d042
petern19800129-ui <petern19800129@gmail.com>
2026-08-29 23:07:42 +0200

    Add native haptic feedback for Sudoku v2

--- sudoku-app/app/src/main/java/com/pete/sudoku/MainActivity.java
+++ sudoku-app/app/src/main/java/com/pete/sudoku/MainActivity.java
@@ -3,7 +3,9 @@ package com.pete.sudoku;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.HapticFeedbackConstants;
 import android.view.View;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -14,9 +16,11 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setStatusBarColor(Color.rgb(13, 17, 23));
+        getWindow().setNavigationBarColor(Color.rgb(13, 17, 23));
 
         webView = new WebView(this);
-        webView.setBackgroundColor(Color.rgb(16, 19, 24));
+        webView.setBackgroundColor(Color.rgb(13, 17, 23));
         webView.setOverScrollMode(View.OVER_SCROLL_NEVER);
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
@@ -24,17 +28,23 @@ public class MainActivity extends Activity {
         settings.setAllowFileAccess(true);
         settings.setBuiltInZoomControls(false);
         settings.setDisplayZoomControls(false);
+        settings.setTextZoom(100);
+        webView.addJavascriptInterface(new NativeBridge(), "Android");
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("file:///android_asset/index.html");
         setContentView(webView);
     }
 
+    private class NativeBridge {
+        @JavascriptInterface
+        public void haptic() {
+            runOnUiThread(() -> webView.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_TAP));
+        }
+    }
+
     @Override
     public void onBackPressed() {
-        if (webView != null && webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            super.onBackPressed();
-        }
+        if (webView != null && webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
     }
 }
